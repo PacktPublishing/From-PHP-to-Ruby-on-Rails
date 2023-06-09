5ac8b1982fc8a5d9ce553a3c91ce6065141bfaeb
Bernardo Pineda <bpineda@zscaler.com>
2023-06-08 19:39:13 -0700

    Set SHA2 password into a variable

--- chapter06/debugable_example.rb
+++ chapter06/debugable_example.rb
@@ -4,8 +4,9 @@ require 'digest'
 user = Hash.new
 user['name'] = 'admin'
 user['password'] = 'admin'
+SECRET_SHA2_PASSWORD = '32b363908ba2382c892800589d6aa3104dc41e6789d2d6a12512c34ec0632834'
 
-if Digest::SHA2.hexdigest(user['password']) == '32b363908ba2382c892800589d6aa3104dc41e6789d2d6a12512c34ec0632834'
+if Digest::SHA2.hexdigest(user['password']) == SECRET_SHA2_PASSWORD
   print "Your password is CORRECT"
 else
   print "Your password is INCORRECT"
