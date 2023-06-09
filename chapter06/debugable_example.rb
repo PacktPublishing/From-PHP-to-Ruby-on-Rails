bef84ada96a33efd373a1232dcb0c59316916b42
Bernardo Pineda <bpineda@zscaler.com>
2023-06-08 21:05:49 -0700

    Added missing space

--- chapter06/debugable_example.rb
+++ chapter06/debugable_example.rb
@@ -6,6 +6,7 @@ user['name'] = 'admin'
 user['password'] = 'secret'
 SECRET_SHA2_PASSWORD = '32b363908ba2382c892800589d6aa3104dc41e6789d2d6a12512c34ec0632834'
 user_sha2_input = Digest::SHA2.hexdigest(user['password'])
+
 if user_sha2_input == SECRET_SHA2_PASSWORD
   print "Your password is CORRECT"
 else
