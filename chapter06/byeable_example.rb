23372d2d68a789eeb6f90c2393ee5d6c07580201
Bernardo Pineda <bpineda@zscaler.com>
2023-06-13 22:47:23 -0700

    Removed .byebug_history

--- chapter06/byeable_example.rb
+++ chapter06/byeable_example.rb
@@ -2,7 +2,7 @@
 require 'byebug'
 
 [1,5,7,9].each do |index|
-  not_label = #{index ? "NOT":""}
+  not_label = index ? "NOT":""
   output = "#{index} is #{not_label} larger than 6"
   puts output
 end
