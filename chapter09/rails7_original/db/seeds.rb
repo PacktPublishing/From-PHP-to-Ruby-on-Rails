c938b5fb3d20e4597ab825e8359e24fec7eae22a
Bernardo Pineda <bpineda@zscaler.com>
2023-08-14 19:54:33 -0700

    Added missing seed file

--- chapter09/rails7_original/db/seeds.rb
+++ chapter09/rails7_original/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Friend.create(first_name: "rasmus", last_name: "lerdorf", email:"rasmus@email.com", phone:"+1(669)1111111")
+Friend.create(first_name: "andi", last_name: "gutmans", email:"andi@email.com", phone:"+1(415)222222222")
+Friend.create(first_name: "zeev", last_name: "suraski", email:"zeev@email.com", phone:"+972 3333333")
+Friend.create(first_name: "yukihiro", last_name: "matsumoto", email:"yukihiro@email.com", phone:"+81 4444444444")
+Friend.create(first_name: "david heinemeier", last_name: "hansson", email:"my5@email.com", phone:"+1(918)5555555")
