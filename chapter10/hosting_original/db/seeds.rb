97fff4929c40dd1ed8feaae254df1256c4c12a38
Bernardo Pineda <bpineda@zscaler.com>
2023-08-29 11:14:59 -0700

    Added original files for hosting

---
+++ chapter10/hosting_original/db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+Friend.create(first_name: "rasmus", last_name: "lerdorf", email:"rasmus@email.com", phone:"+1(669)1111111")
+Friend.create(first_name: "andi", last_name: "gutmans", email:"andi@email.com", phone:"+1(415)222222222")
+Friend.create(first_name: "zeev", last_name: "suraski", email:"zeev@email.com", phone:"+972 3333333")
+Friend.create(first_name: "yukihiro", last_name: "matsumoto", email:"yukihiro@email.com", phone:"+81 4444444444")
+Friend.create(first_name: "david heinemeier", last_name: "hansson", email:"my5@email.com", phone:"+1(918)5555555")
